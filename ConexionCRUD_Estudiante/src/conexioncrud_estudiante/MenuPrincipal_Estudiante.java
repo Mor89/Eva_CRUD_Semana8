c7e26acf5cde630ce56e9f88e654a8a81abc2be1
jennisurio <jennifersurio21@gmail.com>
2022-05-03 09:45:13 -0600

    conexion crud menu

---
+++ ConexionCRUD_Estudiante/src/conexioncrud_estudiante/MenuPrincipal_Estudiante.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexioncrud_estudiante;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+
+public class MenuPrincipal_Estudiante {
+    
+    public static void main(String[] args) throws SQLException {
+        desplegarMenu();
+    }
+    
+   public static void desplegarMenu() throws SQLException{
+       Scanner opcionSeleccionada = new Scanner(System.in);
+       String opcionMenu;
+   }
+   
+  
+     
+}
