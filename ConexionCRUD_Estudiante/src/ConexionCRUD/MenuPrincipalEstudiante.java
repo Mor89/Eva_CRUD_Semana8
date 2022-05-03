0f4cb5724e9d6c8cb0ddb8db7518469cdb1ca4c6
jennisurio <101201572+graciehernandez22@users.noreply.github.com>
2022-05-03 10:20:38 -0600

    1er menu

---
+++ ConexionCRUD_Estudiante/src/ConexionCRUD/MenuPrincipalEstudiante.java
@@ -0,0 +1,16 @@
+
+package ConexionCRUD;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+
+public class MenuPrincipalEstudiante {
+    public static void main(String[] args) throws SQLException {
+        MenuDesplegable();
+    }
+    static void MenuDesplegable()throws SQLException{
+    Scanner opcion = new Scanner (System.in);
+    String MenuOp;
+    }
+}
